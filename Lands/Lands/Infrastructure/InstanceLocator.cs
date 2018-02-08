b10d61b3ef66ffb38afd860d0c6906f4f10b9527
jodaga1992 <jodaga1992@hotmail.com>
2018-02-08 15:29:08 -0500

    Se crea MainViewModel, InstanceLocator, y se agregan logos de redes sociales

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+     class InstanceLocator
+    {
+        #region Propierties
+            public MainViewModel Main
+            {
+                get;
+                set;
+            }
+        #endregion
+
+        #region Constructors
+            public InstanceLocator()
+            {
+            this.Main = new MainViewModel();
+            }
+        #endregion
+    }
+}
